720c4881c4ce6cf0b3634dfef3229efd4d2a5b9f
Geomty <69478549+Geomty@users.noreply.github.com>
2024-03-14 21:05:59 -0400

    cleaned up backend

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// initial stuff
+
 const express = require("express");
 const fs = require("fs");
 const config = require("./config");
@@ -6,22 +8,32 @@ const app = express();
 app.use(express.json());
 app.use(express.static("uploads"));
 express.response.send2 = express.response.status(200).send;
-let files = {};
+
+let pages = {};
 for (const file of fs.readdirSync("./static/").filter(file => file.endsWith(".html"))) {
-    files[file.split(".")[0]] = fs.readFileSync(`./static/${file}`).toString();
+    pages[file.split(".")[0]] = fs.readFileSync(`./static/${file}`).toString();
 }
 
+// auth stuff
+
 app.get("/", (_, res) => {
-    res.send2(files.auth);
-});
-app.post("/", (req, res) => {
-    if(req.body.password != config.password) return res.sendStatus(401);
-    res.send2(files.uploader.replace(/config.password/g, config.password));
+    res.send2(pages.auth);
 });
 
-app.get(`/${config.password}`, (req, res) => {
-    res.send2(loadImages(req));
+app.post("/", (req, res) => {
+    if(req.body.password != config.password) {
+        res.sendStatus(401);
+    } else {
+        res.send2(pages.uploader.replace(/config.password/g, config.password));
+    }
 });
+
+// actual uploading stuff
+
+app.get(`/${config.password}`, (req, res) => {
+    res.send2(loadImages());
+});
+
 app.post(`/${config.password}`, express.text({ limit: "1gb" }), (req, res) => {
     let id;
     let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -30,32 +42,58 @@ app.post(`/${config.password}`, express.text({ limit: "1gb" }), (req, res) => {
         for (let i = 0; i < 5; i++) {
             id += characters.charAt(Math.floor(Math.random() * characters.length));
         }
-    } while (fs.existsSync(`./uploads/${id}`));
+    } while (existsFile(`./uploads/${id}`));
 
     fs.appendFileSync(`./uploads/${id}.png`, Buffer.from(req.body, "base64"));
-    res.status(201).send(loadImages(req));
-});
-app.delete(`/${config.password}`, (req, res) => {
-    if (!fs.existsSync(`./uploads/${req.get("image")}`)) return res.status(400).send("image not found");
-    fs.unlinkSync(`./uploads/${req.get("image")}`);
-    res.send2(loadImages(req));
+    res.status(201).send(loadImages());
 });
 
-app.use("*", (_, res) => res.sendStatus(404));
+app.delete(`/${config.password}`, (req, res) => {
+    if (!existsFile(`./uploads/${req.get("image")}`)) {
+        res.sendStatus(404);
+    } else {
+        fs.unlinkSync(`./uploads/${req.get("image")}`);
+        res.send2(loadImages());
+    }
+});
+
+// other stuff
+
+app.use("/", (_, res) => res.status(404).send(pages["404"]));
+
+app.use("/", (err, req, res, next) => {
+    console.log(err);
+    res.status(500).send(pages["500"]);
+});
 
 app.listen(parseInt(config.port), () => {
     console.log("image uploader is ready");
+    if (!existsDir("./uploads")) fs.mkdirSync("./uploads");
 });
 
-function loadImages(req) {
+function loadImages() {
     let array = [];
     for (const image of fs.readdirSync("./uploads")) {
-        let date = new Date(fs.statSync(`./uploads/${image}`).birthtime).getTime();
-        array.push({ url: `https://${req.get("host")}/${image}`, date: date });
+        array.push({ file: image, date: fs.statSync(`./uploads/${image}`).birthtimeMs });
     }
     array.sort((a, b) => b.date - a.date);
-    for (const object of array) {
-        object.date = new Date(object.date).toString();
-    }
     return array;
+}
+
+function existsFile(file) {
+    try {
+        fs.readFileSync(file);
+    } catch (err) {
+        return false
+    }
+    return true
+}
+
+function existsDir(dir) {
+    try {
+        fs.readdirSync(dir);
+    } catch (err) {
+        return false
+    }
+    return true
 }
